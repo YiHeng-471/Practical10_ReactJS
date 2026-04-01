0b1b9897da063d057dc1640344765b3c171d56fc
GynShen <gynshen@gmail.com>
2026-04-01 10:27:20 +0800

    Policy for action

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -13,31 +13,31 @@ class PostPolicy
 
     public function viewAny(User $user)
     {
-        //
+        return true; // Allow all users to view any post
     }
 
 
     public function view(User $user, Post $post)
     {
-        //
+        return $user->id === $post->user_id; //Only the owner can view the post
     }
 
 
     public function create(User $user)
     {
-        //
+        return $user->role === 'author'; // Only authors can create posts
     }
 
 
     public function update(User $user, Post $post)
     {
-        //
+        return $user->id === $post->user_id; // Only the owner can update the post
     }
 
 
     public function delete(User $user, Post $post)
     {
-        //
+        return $user->id === $post->user_id || $user->role === 'admin'; // Only the owner or admin can delete the post
     }
 
 
