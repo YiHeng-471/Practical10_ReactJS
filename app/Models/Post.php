af30e42d15f9c0af6cd00398e6b6b04841d17b64
CeltYH <yihengchong471@gmail.com>
2026-04-08 09:44:00 +0800

    Refactor Post model and clean up welcome view structure

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    public $timestamps = false;
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id'
+    ];
 }
