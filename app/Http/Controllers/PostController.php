8e2b0b4bc789556f0b1f1eb174613248bebf974e
CeltYH <yihengchong471@gmail.com>
2026-04-08 10:01:11 +0800

    Refactor PostController to streamline methods and improve code clarity

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,54 +1,46 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\User;
 
 class PostController extends Controller
-{
+
+{    
+    /* Display and return all posts from the database.*/
     public function index()
-    {
-        $posts = Post::all();
-        return view('posts.index', ['posts' => $posts]); //posts.index means resources/views/posts/index
+    { 
+        return Post::all();
+    } 
+
+    // Show the form for creating a new post.
+    public function create(Request $request)
+    {       
+        return view('post.create');        
     }
 
-    public function show(Post $post)
-    {
-        $posts = Post::all();
-        return view('posts.show', ['posts' => $posts]);
+    // Store a newly created post in the database.
+    public function store(Request $request)
+    {       
+        return Post::create($request->all());        
     }
 
+    // Update an existing post by its ID.
+    public function update(Request $request, $id)
+    {
+        //If the post does not exist, it will throw a ModelNotFoundException, 
+        // which automatically returns a 404 HTTP response (Page not found).
+        $post=Post::findOrFail($id);
+        $post->update($request->all()); 
+        return $post;
+    }
+
+    // Delete a post by its ID from the database.
     public function destroy($id)
     {
-        $post = Post::findOrFail($id);
+        $post=Post::findOrFail($id);
         $post->delete();
-        return response()->json(['message' => 'Post deleted successfully']);
-    }
-
-    public function create()
-    {
-        if (Gate::allows('isAuthor')) {
-            dd('Author allowed');
-        } else {
-            dd('You are not an Author');
-        }
-    }
-    public function edit()
-    {
-        if (Gate::allows('isAuthor')) {
-            dd('Author allowed');
-        } else {
-            dd('You are not an Author');
-        }
-    }
-    public function delete()
-    {
-        if (Gate::allows('isAdmin') || Gate::allows('isAuthor')) {
-            dd('Admin and Author allowed');
-        } else {
-            dd('You are not Admin and Author');
-        }
+        return 204;
     }
 }
