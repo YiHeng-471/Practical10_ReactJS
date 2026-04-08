f701375eba57024f735f6cc0b637bea008270cd6
CeltYH <yihengchong471@gmail.com>
2026-04-08 09:54:25 +0800

    Refactor Example component to use class syntax and integrate Reactstrap Table

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import {Table, Button} from 'reactstrap';
 
-function Example() {
+export default class Example extends Component {
+    //constructor is a special method used to initialize the state of the class when an instance
+    // of the class is created!
+    constructor()
+    {
+        //To ensure the compoennt is properly initisalized within react's internal functionality!
+        super()
+        //This is an object to hold data that can change over time. It make the applicaation more dynamic!
+        this.state={
+            posts:[]
+        }
+    }
+    render(){
     return (
         <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Content</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>React Post 1</td>
+                        <td>This is the first reactstrap Post </td>
+                        <td>
+                        <Button color="success" size="sm">Edit</Button>
+                        <Button color="danger" size="sm">Delete</Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
         </div>
     );
+    }
 }
-
-export default Example;
 
 if (document.getElementById('example')) {
     ReactDOM.render(<Example />, document.getElementById('example'));
