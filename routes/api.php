77b08920fa064604720acd88da27dbff2626f6f2
CeltYH <yihengchong471@gmail.com>
2026-04-08 10:30:37 +0800

    Add API routes for PostController to manage posts

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/post', [PostController::class, 'store']);
+Route::put('/post/{id}', [PostController::class, 'update']);
+Route::delete('/post/{id}', [PostController::class, 'destroy']);
+
+/* test route using postman
+send get request: http://localhost:8000/api/posts
+send post request: http://localhost:8000/api/post
+send put request: http://localhost:8000/api/post/3
+send delete request: http://localhost:8000/api/post/3
+*/
